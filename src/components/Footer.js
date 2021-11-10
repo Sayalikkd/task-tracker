14737a26d0f514e31505c49be0c6f3942be4292b
Sayalikkd <sayalikkd@gmail.com>
2021-11-10 21:13:28 +0100

    Task tracker app

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+
+// const Footer = () => {
+//     return (
+//         <footer>
+//            <p> React Example - Sayali Kakade</p> 
+//            <a href="/about">About </a>
+//         </footer>
+//     )
+// }
+
+// export default Footer
